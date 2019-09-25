10869c685f9f32a3961aa2f167057b9682bc990b
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-09-25 21:36:03 +0200

    Homework - renaming fields in tables

--- homework.sql
+++ homework.sql
@@ -59,72 +59,72 @@ DROP TABLE pracownicy2;
 -- Punkt 9.Utworzenie tabeli stanowiska
 
 CREATE TABLE stanowiska (
-sta_id INT AUTO_INCREMENT PRIMARY KEY,
-sta_nazwa VARCHAR(15),
-sta_opis VARCHAR(15),
-sta_wyplata DECIMAL(7,2)
+id INT AUTO_INCREMENT PRIMARY KEY,
+nazwa VARCHAR(15),
+opis VARCHAR(15),
+wyplata DECIMAL(7,2)
 );
 
 -- Punkt 10.Utworzenie tabeli adresy
 
 CREATE TABLE adresy (
-adr_id INT AUTO_INCREMENT PRIMARY KEY,
-adr_ul VARCHAR(15),
-adr_numer_domu INT,
-adr_numer_mieszkania INT,
-adr_kod VARCHAR(5),
-adr_miasto VARCHAR(15)
+id INT AUTO_INCREMENT PRIMARY KEY,
+ulica VARCHAR(15),
+numer_domu INT,
+numer_mieszkania INT,
+kod VARCHAR(5),
+miasto VARCHAR(15)
 );
 
 -- Punkt 11.Utworzenie tabeli pracownicy
 
 CREATE TABLE pracownicy (
-prac_id INT AUTO_INCREMENT PRIMARY KEY,
-prac_imie VARCHAR(15),
-prac_nazwisko VARCHAR(15),
-prac_adr_id INT,
-prac_sta_id INT
+id INT AUTO_INCREMENT PRIMARY KEY,
+imie VARCHAR(15),
+nazwisko VARCHAR(15),
+adres_id INT,
+stanowisko_id INT
 );
 
 -- Punkt 12. Dodanie danych testowych
 
 -- tabela stanowiska
-INSERT INTO stanowiska(sta_nazwa, sta_opis, sta_wyplata) VALUES ('ACCOUNTS', 'Księgowość', 3500);
-INSERT INTO stanowiska(sta_nazwa, sta_opis, sta_wyplata) VALUES ('IT', 'Informatyka', 13500);
-INSERT INTO stanowiska(sta_nazwa, sta_opis, sta_wyplata) VALUES ('MARKETING', 'Dział handlu', 7500);
-INSERT INTO stanowiska(sta_nazwa, sta_opis, sta_wyplata) VALUES ('MANAGEMENT', 'Zarząd/dyrekcja', 17500);
+INSERT INTO stanowiska(nazwa, opis, wyplata) VALUES ('ACCOUNTS', 'Księgowość', 3500);
+INSERT INTO stanowiska(nazwa, opis, wyplata) VALUES ('IT', 'Informatyka', 13500);
+INSERT INTO stanowiska(nazwa, opis, wyplata) VALUES ('MARKETING', 'Dział handlu', 7500);
+INSERT INTO stanowiska(nazwa, opis, wyplata) VALUES ('MANAGEMENT', 'Zarząd/dyrekcja', 17500);
 
 -- tabela adresy
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Wiejska', 1, 12, 12345, 'Warszawa');
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Strzelecka', 123, 15, 78900, 'Opole');
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Strzegomska', 45, 15, 56678, 'Wrocław');
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Podwale', 12, 1, 56678, 'Wrocław');
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Podwale', 12, 4, 56678, 'Wrocław');
-INSERT INTO adresy(adr_ul, adr_numer_domu, adr_numer_mieszkania, adr_kod, adr_miasto) VALUES ('Wrosławska', 9, 2, 34678, 'Legnica');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Wiejska', 1, 12, 12345, 'Warszawa');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Strzelecka', 123, 15, 78900, 'Opole');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Strzegomska', 45, 15, 56678, 'Wrocław');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Podwale', 12, 1, 56678, 'Wrocław');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Podwale', 12, 4, 56678, 'Wrocław');
+INSERT INTO adresy(ulica, numer_domu, numer_mieszkania, kod, miasto) VALUES ('Wrosławska', 9, 2, 34678, 'Legnica');
 
 -- tabela pracownik
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Jan', 'Kowalski', 1, 1);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Katerzyna', 'Nowak', 2, 2);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Ksawery', 'Zawadzki', 3, 2);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Teodor', 'Nowakowski', 4, 1);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Malwina', 'Kowalska', 1, 4);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Zbigniew', 'Walendziak', 5, 3);
-INSERT INTO pracownicy(prac_imie, prac_nazwisko, prac_adr_id, prac_sta_id) VALUES ('Teresa', 'Markowska', 6, 3);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Jan', 'Kowalski', 1, 1);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Katerzyna', 'Nowak', 2, 2);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Ksawery', 'Zawadzki', 3, 2);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Teodor', 'Nowakowski', 4, 1);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Malwina', 'Kowalska', 1, 4);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Zbigniew', 'Walendziak', 5, 3);
+INSERT INTO pracownicy(imie, nazwisko, adres_id, stanowisko_id) VALUES ('Teresa', 'Markowska', 6, 3);
 
 -- Punkt 13. Pełne dane o pracowniku
 
-SELECT p.prac_imie, p.prac_nazwisko, s.sta_nazwa, s.sta_wyplata, a.adr_ul, a.adr_numer_domu, a.adr_numer_mieszkania, a.adr_miasto, a.adr_kod 
+SELECT p.imie, p.nazwisko, s.nazwa, s.wyplata, a.ulica, a.numer_domu, a.numer_mieszkania, a.miasto, a.kod 
 FROM pracownicy p 
-JOIN adresy a ON p.prac_adr_id = a.adr_id
-JOIN stanowiska s ON p.prac_sta_id = s.sta_id;
+JOIN adresy a ON p.adres_id = a.id
+JOIN stanowiska s ON p.stanowisko_id = s.id;
 
 -- Punkt 14. Suma wypłat pracowników w firmie
 
-SELECT SUM(s.sta_wyplata) FROM pracownicy p
-JOIN stanowiska s ON p.prac_sta_id = s.sta_id; 
+SELECT SUM(s.wyplata) FROM pracownicy p
+JOIN stanowiska s ON p.stanowisko_id = s.id; 
 
 -- Punkt 15. Pracownicy zamieszkali pod wskazanym kodem pocztowm (56678)
 
 SELECT * FROM pracownicy p
-JOIN adresy a ON p.prac_adr_id = a.adr_id
-WHERE a.adr_kod = '56678'
+JOIN adresy a ON p.adres_id = a.id
+WHERE a.kod = '56678'
